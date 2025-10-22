420e8367afe1e4d93b5d66f12be1da7e29f321a3
Al Shahreyaj <49917196+alshahreyaj@users.noreply.github.com>
2025-10-22 21:22:34 +0600

    updated games page.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,8 @@
- 
+ document.addEventListener('DOMContentLoaded', function () {
+  if (window.bootstrap) {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+      new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }
+});
